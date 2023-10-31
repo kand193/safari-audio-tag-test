d8b811afb9458982d69aeb050bab0340f524aaa5
Youngjin Park <qkrdudwls99@gmail.com>
2023-10-31 05:03:38 +0000

    fix: 타이머 추가

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,18 +1,29 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 export const AudioPlayer = (props) => {
     const ref = useRef();
+    const [time, setTime] = useState(0);
 
     const handlePlay = () => {
-        if (ref.current) {
-            ref.current.play?.();
-        }
+        setTimeout(() => {
+            if (ref.current) {
+                ref.current.play?.();
+            }    
+        }, time * 1000);
     }
 
     return (
-        <>
+        <div style={{ display: 'flex' }}>
+            <input
+                type="number"
+                value={time}
+                min={0}
+                onChange={(e) => setTime(e.target.value)}
+                style={{ width: '50px' }}
+            />
+            초 뒤
             <button onClick={handlePlay}>재생</button>
             <audio {...props} ref={ref} />
-        </>
+        </div>
     )
 }
