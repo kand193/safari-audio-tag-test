2a5c2fe20c46eb0656aa5f5b25a42cfdb30c5d7e
Youngjin Park <qkrdudwls99@gmail.com>
2023-11-02 10:20:38 +0000

    fix: 인터랙션용 Canvas 추가

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <canvas width="300" height="300" style={{ border: '1px solid white' }} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
